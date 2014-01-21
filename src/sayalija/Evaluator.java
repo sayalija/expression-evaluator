9aaf0735d6d3fccf8b715b9189a6818698e09ea6
unknown <sayalija@Insayalija.corporate.thoughtworks.com>
2014-01-21 19:00:44 +0530

    expression working with spaces for release0.1

--- src/sayalija/Evaluator.java
+++ src/sayalija/Evaluator.java
@@ -1,30 +1,22 @@
 package sayalija;
 
+import java.util.ArrayList;
+import java.lang.String;
+
 public class Evaluator {
-    int result = 0;
-    String num1, num2, operator;
-    int index;
 
     public String evaluate(String expr) {
-        if(true == isInteger(expr))
-            return expr;
-        index =expr.indexOf('+');
-        num1 = expr.substring(0,index);
-        System.out.println(num1);
-        operator = String.valueOf(expr.charAt(index));
-        System.out.println(operator);
-        num2 = evaluate(expr.substring(index+1));
-        System.out.println(num2);
-        if (0 == "+".compareTo(operator))
-            result = Integer.parseInt(num1) + Integer.parseInt(num2);
-        return String.valueOf(result);
-    }
-    public static boolean isInteger(String str) {
-        try {
-            Integer.parseInt(str);
-            return true;
-        } catch (NumberFormatException nfe) {
-            return false;
+        int result = 0;
+        ArrayList<Integer> operands = new ArrayList<Integer>(5);
+        String[] expression = expr.split(" ");
+        for (int i = 0; i < expression.length; i = i + 2) {
+            operands.add(Integer.valueOf(expression[i]));
         }
+        if (expr.contains("+")) {
+            for (Integer operand : operands) {
+                result = result + operand;
+            }
+        }
+        return String.valueOf(result);
     }
 }
--- test/EvaluatorTest.java
+++ test/EvaluatorTest.java
@@ -9,7 +9,7 @@ public class EvaluatorTest {
         String actual;
         String expected = "5";
 
-        actual = eval.evaluate("2+3");
+        actual = eval.evaluate("2 + 3");
 
         Assert.assertEquals(expected, actual);
     }
@@ -20,10 +20,30 @@ public class EvaluatorTest {
         String actual;
         String expected = "5";
 
-        actual = eval.evaluate("2+2+1");
+        actual = eval.evaluate("2 + 2 + 1");
 
         Assert.assertEquals(expected, actual);
     }
 
-    
+    @Test
+    public void testGives7ForAdditionOf2And4And1() throws Exception {
+        Evaluator eval = new Evaluator();
+        String actual;
+        String expected = "7";
+
+        actual = eval.evaluate("2 + 4 + 1");
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGives14ForAdditionOf10And2And1And1() throws Exception {
+        Evaluator eval = new Evaluator();
+        String actual;
+        String expected = "14";
+
+        actual = eval.evaluate("10 + 2 + 1 + 1");
+
+        Assert.assertEquals(expected, actual);
+    }
 }
