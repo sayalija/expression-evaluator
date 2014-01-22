189d22e891c42aaf79a8c8ec63cd6ef8852e63cf
unknown <sayalija@Insayalija.corporate.thoughtworks.com>
2014-01-22 17:47:30 +0530

    working for multiple operators on two numbers

--- src/sayalija/Evaluator.java
+++ src/sayalija/Evaluator.java
@@ -2,10 +2,13 @@ package sayalija;
 
 import java.util.ArrayList;
 import java.lang.String;
+import java.util.Stack;
+
+import static sayalija.Evaluator.calculate;
 
 public class Evaluator {
 
-    public String evaluate(String expr) {
+    public String evaluate1(String expr) {
         int result = 0;
         ArrayList<Integer> operands = new ArrayList<Integer>(5);
         String[] expression = expr.split(" ");
@@ -19,4 +22,49 @@ public class Evaluator {
         }
         return String.valueOf(result);
     }
+
+    public String evaluate(String expr) {
+        int result = 0;
+        int num1, num2;
+        char operator;
+        Stack<Integer> operands = new Stack<Integer>();
+        Stack<Character> operators = new Stack<Character>();
+        String[] expression = expr.split(" ");
+        for (int i = 0; i < expression.length; i = i + 2) {
+            operands.push(Integer.valueOf(expression[i]));
+        }
+        for (int i = 1; i < expression.length; i = i + 2) {
+            operators.push(expression[i].charAt(0));
+        }
+
+        do {
+            num1 = operands.pop();
+            operator = operators.pop();
+            num2 = operands.pop();
+
+            operands.push(calculate(num1, num2, operator));
+
+        } while (false == operators.empty() && false == operands.empty());
+
+        result = operands.pop();
+        return String.valueOf(result);
+
+    }
+
+    static int calculate(int num1, int num2, char operator) {
+        switch (operator) {
+            case '+':
+                return num2 + num1;
+            case '-':
+                return num2 - num1;
+            case '*':
+                return num2 * num1;
+            case '/':
+                return num2 / num1;
+            case '^':
+                return (int) Math.pow(num2,num1);
+        }
+        return 0;
+    }
+
 }
--- test/EvaluatorTest.java
+++ test/EvaluatorTest.java
@@ -46,4 +46,49 @@ public class EvaluatorTest {
 
         Assert.assertEquals(expected, actual);
     }
+
+    @Test
+    public void testForSubtractionOfTwoNumbers() throws Exception {
+        Evaluator eval = new Evaluator();
+        String actual;
+        String expected = "4";
+
+        actual = eval.evaluate("6 - 2");
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testForMultiplicationOfTwoNumbers() throws Exception {
+        Evaluator eval = new Evaluator();
+        String actual;
+        String expected = "24";
+
+        actual = eval.evaluate("3 * 8");
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testForDivisionOfTwoNumbers() throws Exception {
+        Evaluator eval = new Evaluator();
+        String actual;
+        String expected = "4";
+
+        actual = eval.evaluate("12 / 3");
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGives64For8PowerOf2() throws Exception {
+        Evaluator eval = new Evaluator();
+        String actual;
+        String expected = "64";
+
+        actual = eval.evaluate("8 ^ 2");
+
+        Assert.assertEquals(expected, actual);
+    }
+
 }
